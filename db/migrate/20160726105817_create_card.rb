5f64ef04bc975aa389a6125187a60e496e0a6808
Alex Bills <email": null>
2016-07-28 09:38:18 -0400

    added model/migration for trick and updated card migration

--- db/migrate/20160726105817_create_card.rb
+++ db/migrate/20160726105817_create_card.rb
@@ -3,6 +3,8 @@ class CreateCard < ActiveRecord::Migration
     create_table :cards do |t|
       t.string :suit
       t.string :value
+      t.boolean :played, default: false
+      t.belongs_to :trick
       t.belongs_to :deck
       t.belongs_to :hand
 
