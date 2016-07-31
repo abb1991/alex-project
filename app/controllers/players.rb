b9ed7af6e9db8ce704ed2604b35c9de6249c8ca9
abb1991 <alexbills1991@gmail.com>
2016-07-31 11:27:22 -0400

    refined game logic in show file

--- app/controllers/players.rb
+++ app/controllers/players.rb
@@ -1,9 +1,8 @@
 get '/players/:id' do
-
-  p_1 = Player.new(game_id: params[:id], user_id: 1 )
-  p_2 = Player.new(game_id: params[:id], user_id: 2 )
-  p_3 = Player.new(game_id: params[:id], user_id: 3 )
-  p_4 = Player.new(game_id: params[:id], user_id: 4 )
+  p_1 = Player.new(game_id: params[:id], user_id: 1, color: 'red' )
+  p_2 = Player.new(game_id: params[:id], user_id: 2, color: 'blue' )
+  p_3 = Player.new(game_id: params[:id], user_id: 3, color: 'green' )
+  p_4 = Player.new(game_id: params[:id], user_id: 4, color: 'orange')
   if p_1.save && p_2.save && p_3.save && p_4.save
     redirect "/rounds/#{params[:id]}/new"
   else
